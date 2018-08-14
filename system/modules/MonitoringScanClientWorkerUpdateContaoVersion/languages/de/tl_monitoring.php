fb1f9a2a445c765760cebab7e4c4b8c5d2e72158
Cliff Parnitzky <github@cliff-parnitzky.de>
2018-08-15 00:10:08 +0200

    Added storing and displaying the system update history (closes #2)

---
+++ system/modules/MonitoringScanClientWorkerUpdateContaoVersion/languages/de/tl_monitoring.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2018 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Cliff Parnitzky 2018-2018
+ * @author     Cliff Parnitzky
+ * @package    MonitoringScanClientWorkerUpdateContaoVersion
+ * @license    LGPL
+ */
+
+/**
+ * Labels
+ */
+$GLOBALS['TL_LANG']['tl_monitoring']['systemUpdateHistory_buttonActive']            = 'Historie der System-Aktualisierung anzeigen';
+$GLOBALS['TL_LANG']['tl_monitoring']['systemUpdateHistory_buttonInactive']          = 'Es steht keine Historie der System-Aktualisierung zur Verfügung';
+$GLOBALS['TL_LANG']['tl_monitoring']['systemUpdateHistory_dialogTitle']             = 'Historie der System-Aktualisierung';
+$GLOBALS['TL_LANG']['tl_monitoring']['systemUpdateHistory_dialogTableHeaderDate']   = 'Aktualisiert am';
+$GLOBALS['TL_LANG']['tl_monitoring']['systemUpdateHistory_dialogTableHeaderSystem'] = 'Neues System';
+
+?>
